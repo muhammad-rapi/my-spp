9e559ebab2f6aca16892a82da7a3b9e5a7c7c7c9
rafi <admin@MacBook-Pro-admin.local>
2023-11-06 20:47:34 +0700

    menambahkan beberapa fitur detail siswa

--- resources/views/students/detail-student.blade.php
+++ resources/views/students/detail-student.blade.php
@@ -10,63 +10,37 @@
                 <div class="card-header pb-0">
                     <div class="d-flex flex-row justify-content-between">
                         <div>
-                            <h5 class="mb-0">List Siswa</h5>
+                            <h5 class="mb-0">Detail Siswa</h5>
                         </div>
-                        <a href="/create-student" class="btn bg-gradient-primary btn-sm mb-0" type="button">+&nbsp; Tambah Siswa</a>
                     </div>
-                    @if(session('success'))
-                        <div class="m-3  alert alert-success alert-dismissible fade show" id="alert-success" role="alert">
-                            <span class="alert-text text-white">
-                            {{ session('success') }}</span>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
-                                <i class="fa fa-close" aria-hidden="true"></i>
-                            </button>
-                        </div>
-                    @endif
                 </div>
                 <div class="card-body px-0 pt-0 pb-2">
-                    <div class="table-responsive p-0">
-                        <table class="table align-items-center mb-0">
-                            <thead>
-                                <tr>                            
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Name
-                                    </th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Jurusan
-                                    </th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Kelas
-                                    </th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        NIS
-                                    </th>
-                                    <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                                        Alamat
-                                    </th>                                    
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td class="text-center">
-                                        <p class="text-sm font-weight-bold mb-0">{{ $student->name }}</p>
-                                    </td>
-                                    <td class="text-center">
-                                        <p class="text-sm font-weight-bold mb-0">{{ $student->major->name }}</p>
-                                    </td>                                
-                                    <td class="text-center">
-                                        <p class="text-sm font-weight-bold mb-0">{{ $student->class }}</p>
-                                    </td>                                
-                                    <td class="text-center">
-                                        <p class="text-sm font-weight-bold mb-0">{{ $student->nis}}</p>
-                                    </td>                                
-                                    <td class="text-center">
-                                        <p class="text-sm font-weight-bold mb-0">{{ $student->address}}</p>
-                                    </td>                                      
-                                </tr>    
-                            </tbody>
-                        </table>
-                    </div>
+                    <div class="card">
+                        <div class="card-body pt-2">
+                            <p class="card-title h5 d-block text-darker mt-5 mb-3">
+                                {{  $student->name}}
+                            </p>                           
+                            <p class="card-description mb-4">
+                                Kelas : {{  $student->class}}
+                            </p>
+                            <p class="card-description mb-4">
+                                Jurusan : {{  $student->major->name}}
+                            </p>
+                            <p class="card-description mb-4">
+                                NIS : {{  $student->nis}}
+                            </p>
+                            <p class="card-description mb-4">
+                                Alamat : {{  $student->address}}
+                            </p>
+                        {{-- <div class="author align-items-center">                        
+                            <div class="name ps-3">
+                            <span>Mathew Glock</span>
+                            <div class="stats">
+                                <small>Posted on 28 February</small>
+                            </div>
+                            </div>
+                        </div> --}}
+                    </div>                
                 </div>
             </div>
         </div>
